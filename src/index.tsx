24c9e2e5aaa13884f2b06fd225773d50b30fefb1
Alexander <65725016+chillhaze@users.noreply.github.com>
2022-03-21 13:43:21 +0200

    index.tsx updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+// import "./files/01-primitives";
+// import "./files/02-arrays";
+// import "./files/03-types";
+// import "./files/04-interfaces";
+// import "./files/05-enums";
+// import "./files/06-functions";
+// import "./files/07-classes";
 
 ReactDOM.render(
   <React.StrictMode>

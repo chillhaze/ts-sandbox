91d5bcadee409a1e18473792894ea391d916cd04
Alexander <65725016+chillhaze@users.noreply.github.com>
2022-04-08 19:23:35 +0300

    added prep files

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import "./files/01-primitives";
-// import "./files/02-arrays";
-// import "./files/03-types";
-// import "./files/04-interfaces";
-// import "./files/05-enums";
-// import "./files/06-functions";
-// import "./files/07-classes";
-import './files/08-generics';
+import './files/01-generics';
 
 ReactDOM.render(
   <React.StrictMode>

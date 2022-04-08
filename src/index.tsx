daab78fe5dc35e2b782d3ab7f56d5e5543f0384a
Alexander <65725016+chillhaze@users.noreply.github.com>
2022-04-08 16:46:51 +0300

    index.tsx fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-// import "./files/01-primitives";
-// import "./files/02-arrays";
-// import "./files/03-types";
-// import "./files/04-interfaces";
-// import "./files/05-enums";
-// import "./files/06-functions";
-// import "./files/07-classes";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>hello</div>
+    <div>Hello TypeScript!</div>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
